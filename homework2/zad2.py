52047e3efbb0cd8ca08f25a092cac0ab2a2e36d3
Timur <greenteamat@yandex.ru>
2024-10-12 14:28:42 +0500

    processing

--- homework2/zad2.py
+++ homework2/zad2.py
@@ -1,6 +1,11 @@
+squares = []
 a = input("Введите первое число: ").replace(',','.')
 b = input("Введите второе число: ").replace(',','.')
-squares = []
-for i in range(round(a), b):
-    squares.append(i**2)
-print(f"Список квадратов целых чисел между {a} и {b}: {squares}")
+if a.isalpha()==True or b.isalpha()==True:
+    print('ошибка!')
+elif a.isnumeric()==True and a.isnumeric()==True:
+    a = int(a); b = int(b)
+    a +=1
+    for i in range(a, b):
+        squares.append(i**2)
+print(f"Список квадратов целых чисел между {a-1} и {b}: {squares}")
