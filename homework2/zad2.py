3a0be8f751688bff3bb1c32c61e2b2d4799b579c
Timur <greenteamat@yandex.ru>
2024-10-13 17:55:31 +0500

    Dont know

--- homework2/zad2.py
+++ homework2/zad2.py
@@ -1,11 +1,11 @@
+a = float(input("Введите первое число: "))#.replace(',','.')
+b = float(input("Введите второе число: "))#.replace(',','.')
+#if a.isalpha()==True or b.isalpha()==True:
+#    print('ошибка!')
+#else:
+c = int(a) + (1 if a > int(a) else 0)
+d = int(b) + (1 if b > int(b) else 0)
 squares = []
-a = input("Введите первое число: ").replace(',','.')
-b = input("Введите второе число: ").replace(',','.')
-if a.isalpha()==True or b.isalpha()==True:
-    print('ошибка!')
-elif a.isnumeric()==True and a.isnumeric()==True:
-    a = int(a); b = int(b)
-    a +=1
-    for i in range(a, b):
-        squares.append(i**2)
-print(f"Список квадратов целых чисел между {a-1} и {b}: {squares}")
+for i in range(c,d):
+    squares.append(i**2)
+print(f"Список квадратов целых чисел между {float(a)} и {float(b)}: {squares}")
