a1eafe4d8fd63486811963652446bc7b100585b8
Pomidor12 <73022500+Pomidor12@users.noreply.github.com>
2024-12-18 23:12:13 +0500

    Create zad9.py

---
+++ homework9/zad9.py
@@ -0,0 +1,19 @@
+import re
+
+def parse_email(email):
+    pattern = r'^(?P<username>[a-zA-Z][\w.-]*)@(?P<domain>[\w.-]+\.\w+)$'
+    match = re.match(pattern, email)
+
+    if match:
+        username = match.group('username')
+        domain = match.group('domain')
+        return username, domain
+    else:
+        return None, None
+
+email_input = input("Введите ваш email: ")
+username, domain = parse_email(email_input)
+if username and domain:
+    print(f"Имя пользователя: {username}, Домен: {domain}")
+else:
+    print("Некорректный email. Пожалуйста, попробуйте снова.")
